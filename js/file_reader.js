e666cb258b5aaaa51a1da543fa6b92d6a0d7a19c
taise <taise515@gmail.com>
2013-05-03 22:06:23 +0900

    output multi file text

--- js/file_reader.js
+++ js/file_reader.js
@@ -2,7 +2,8 @@
 
 // file reader
 $(function(){
-  function fileSelect(evt) {
+  //function fileSelect(evt) {
+    var fileSelect = function(evt) {
     var files = evt.target.files; // FileList object
 
     // files is a FileList of File objects.
@@ -12,26 +13,36 @@ $(function(){
       fileReader(f);
     }
     $('#list').html('<ul>' + output.join('</ul><ul>') + '</ul>');
-  }
+  };
 
-  $('#files').change(fileSelect);
+  $('#files').change(function(evt){
+   fileSelect(evt);
+   extractData(evt);
+ });
 
   var fileInfo = function(file){
-    var infos = []
-  infos.push(
-    'File name: ' + escape(file.name),
-    'File type: ' + (file.type || 'N/A'),
-    'File size: ' + file.size + ' bytes,',
-    'last modified: ' + file.lastModifiedDate.toLocaleDateString()
-    );
+    var infos = [];
+    infos.push(
+               'File name: ' + escape(file.name),
+               'File type: ' + (file.type || 'N/A'),
+               'File size: ' + file.size + ' bytes,',
+               'last modified: ' + file.lastModifiedDate.toLocaleDateString()
+               );
     return '<li>' + infos.join('</li><li>') + '</li>';
   };
 
   var fileReader = function(file, parentTag) {
     var reader = new FileReader();
     reader.onload = function (evt) {
-      $('#fileText').html(reader.result);
+      $('#csvData').append("<pre>" + reader.result + "</pre>");
     };
     reader.readAsText(file, 'shift-jis');
   };
+
+  // data extraction
+  var extractData = function(evt){
+    var csv = evt.target;
+    console.log(csv);
+  };
+
 });
--- js/precipitation.js
+++ js/precipitation.js
@@ -1,3 +1,43 @@
 'use strict'
 
+// graph area
+var h = 300;
+var w = 100;
+var barPadding = 1;
 
+var dataset = [10, 50, 100, 20, 70];
+var len = dataset.length;
+var svg = d3.select("#graph")
+.append("svg")
+.attr("width", w)
+.attr("height", h)
+
+// set rect size
+var setSizeGraph = svg.selectAll("rect")
+.data(dataset)
+.enter()
+.append("rect")
+.attr("x", function(d, i) { return i * (w / len) })
+.attr("y", function(d) { return h - (d * 3) });
+
+// graphing each data of dataset
+var graphing = setSizeGraph
+.attr("width", w / len - barPadding)
+.attr("height", function(d) { return d * 3; });
+
+// coloring
+graphing.attr("fill", function(d) {
+  return "rgb(" + (d * 2.5) + ", " + (d * 5) + ", 0)";
+});
+
+// data extraction
+$(function(){
+  function extractData(evt){
+    var csv = evt.target.text;
+    console.log(1);
+  };
+
+  var csvData = $('#csvData');
+  csvData.on("change", "pre", function(evt){ console.log("change!"); });
+  console.log(csvData);
+});
