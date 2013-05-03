af2e3e4c600d7148478736b704455024a87275bf
taise <taise515@gmail.com>
2013-05-03 13:21:56 +0900

    local file selecter

---
+++ js/precipitation.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// file reader
+function handleFileSelect(evt) {
+  var files = evt.target.files; // FileList object
+
+  // files is a FileList of File objects. List some properties.
+  var output = [];
+  for (var i = 0, f; f = files[i]; i++) {
+    output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+        f.size, ' bytes, last modified: ',
+        f.lastModifiedDate.toLocaleDateString(), '</li>');
+  }
+  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+
+
+
+
+
+// 幅（ Width ）と高さ（ height ）
+var w = 500;
+var h = 100;
+var barPadding = 1;
+
